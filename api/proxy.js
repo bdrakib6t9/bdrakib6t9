71ac467b172e86a4560af1cf49ebe1f0e0eb753f
bdrakib6t9 <bdrakib6t9@gmail.com>
2026-04-19 03:40:49 +0600

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  try {
+    const { url } = req.query;
+
+    if (!url) {
+      return res.status(400).send("No URL provided");
+    }
+
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "*/*"
+      }
+    });
+
+    const contentType = response.headers.get("content-type") || "";
+
+    if (contentType.includes("application/json")) {
+      const data = await response.json();
+      return res.status(200).json(data);
+    } else {
+      const text = await response.text();
+      return res.status(200).send(text);
+    }
+
+  } catch (err) {
+    res.status(500).send("Proxy Error");
+  }
+}
